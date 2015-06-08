38cad246b0e6d9a16877583ec5ad292ce48b51b9
Eugene Krevenets <hyzhak@ya.ru>
2015-06-08 21:10:22 +0200

    replace tabs with spaces

--- spec/toHaveMethodSpec.js
+++ spec/toHaveMethodSpec.js
@@ -1,23 +1,23 @@
 require('../index');
 
-describe('toHaveMethod matcher', function() {
-	it('should be defined in Expectation', function() {
-		expect( expect().toHaveMethod).toBeDefined();
-	});
+describe('toHaveMethod matcher', function () {
+    it('should be defined in Expectation', function () {
+        expect(expect().toHaveMethod).toBeDefined();
+    });
 
-	it('should be function in Expectation', function() {
-		expect( typeof expect().toHaveMethod ).toEqual('function');
-	});
+    it('should be function in Expectation', function () {
+        expect(typeof expect().toHaveMethod).toEqual('function');
+    });
 
-	it('should not apply pure property', function() {
-		expect({ x: 10 }).not.toHaveMethod('x');
-	});
+    it('should not apply pure property', function () {
+        expect({x: 10}).not.toHaveMethod('x');
+    });
 
-	it('should check whether is function', function() {
-		expect({ m: function() {}}).toHaveMethod('m');
-	});
+    it('should check whether is function', function () {
+        expect({ m: function () {}}).toHaveMethod('m');
+    });
 
-	it('should check whether is function', function() {
-		expect(null).not.toHaveMethod('m');
-	});
+    it('should check whether is function', function () {
+        expect(null).not.toHaveMethod('m');
+    });
 });
